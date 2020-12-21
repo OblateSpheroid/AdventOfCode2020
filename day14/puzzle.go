c86472d1fc693006e0ca8d283cf110f02c692b75
js-g <oblatespheroid@protonmail.com>
2020-12-21 16:52:40 -0500

    add day 14 part 2

--- day14/puzzle.go
+++ day14/puzzle.go
@@ -12,6 +12,22 @@ type Card struct {
 	inst [][2]int // memory, value pairs
 }
 
+func transpose(a [][]rune) [][]rune {
+	// transpose a "matrix"
+	if len(a) == 0 {
+		return a
+	}
+	t := [][]rune{}
+	for i := 0; i < len(a[0]); i++ {
+		tmp := make([]rune, len(a))
+		for j := range a {
+			tmp = append(tmp, a[j][i])
+		}
+		t = append(t, tmp)
+	}
+	return t
+}
+
 func toBase2(i int) string {
 	// string representing integer in base 2
 	s := strconv.FormatInt(int64(i), 2)
@@ -62,8 +78,80 @@ func count(m map[int]int) int {
 	return c
 }
 
+/* Part 2 */
+func maskv2(m, v string) []rune {
+	// apply mask using v2 rules
+	n := []rune(v) // can't modify string in place
+	for i := 0; i < len(m); i++ {
+		if m[i] != '0' {
+			n[i] = rune(m[i]) // will produce runes with 'X'
+		}
+	}
+	return n
+}
+
+func toBase10v2(s []rune) []int {
+	// convert to all possible integers
+	all := [][]rune{}
+	for i, v := range s {
+		if v == 'X' {
+			if i == 0 {
+				all = append(all, []rune("01"))
+				continue
+			}
+			zeros := strings.Repeat("0", len(all[i-1]))
+			ones := strings.Repeat("1", len(all[i-1]))
+			all = append(all, []rune(zeros+ones))
+			for j := range all[:i] {
+				all[j] = append(all[j], all[j]...) // go back and double previous
+			}
+		} else {
+			if i == 0 {
+				all = append(all, []rune{v})
+				continue
+			}
+			new := strings.Repeat(string(v), len(all[i-1]))
+			all = append(all, []rune(new))
+		}
+	}
+	t := transpose(all)
+	// for each possibility, produce an integer it represents
+	n := []int{}
+	for _, v := range t {
+		n = append(n, toBase10(v))
+	}
+	return n
+}
+
+func processv2(cs []Card) map[int]int {
+	// mask memory address
+	mem := make(map[int]int)
+	for _, c := range cs {
+		for _, i := range c.inst {
+			s := toBase2(i[0])
+			m := maskv2(c.mask, s)
+			ints := toBase10v2(m)
+			for _, j := range ints {
+				mem[j] = i[1]
+			}
+		}
+	}
+	return mem
+}
+
 func main() {
+	test1 := parseFile("test1.txt")
+	t := process(test1)
+	fmt.Println(count(t) == 165)
+
+	test2 := parseFile("test2.txt")
+	t2 := processv2(test2)
+	fmt.Println(count(t2) == 208)
+
 	data := parseFile("data.txt")
 	m := process(data)
-	fmt.Println(count(m))
+	fmt.Printf("Answer 1: %d\n", count(m))
+
+	m2 := processv2(data)
+	fmt.Printf("Answer 2: %d\n", count(m2))
 }
